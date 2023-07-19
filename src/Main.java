29913713b664da81f0f4393d2e386f04990af3f5
Nitrogen <nhtran@iastate.edu>
2023-07-19 13:25:22 -0500

    Test2

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Car car = new Car(4, 2, "NEW CAR BRAND");
 
-    static int fib(int n)
-    {
-        int a = 0, b = 1, c;
-        if (n == 0)
-            return a;
-        for (int i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
-    }
-
-    static int h(int n)
-    {
-        int a = 0, b = 1, c;
-        if (n == 0)
-            return a;
-        for (int i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
+        System.out.println(car.getBrand());
     }
 }
 
